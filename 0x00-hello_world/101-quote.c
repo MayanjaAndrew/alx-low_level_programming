f3b6fc29b540b36ff2240321d0ddf5916bdeb0c9
MayanjaAndrew <andremayanja@gmail.com>
2022-03-10 07:48:11 -0800

    Add task 8 of hello c progamming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/** 
- * main - prints exactly and that piece of art is useful
- * Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+/**
+ * main - prints exactly and that piece of art is useful Dor Korpar,
+ * 2015-10-19,
+ * followed by a new line, to the standarrd error.
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
