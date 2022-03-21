ac7c0503d044228b9cc1a7f2f7016b95972bbd4e
MayanjaAndrew <andremayanja@gmail.com>
2022-03-20 22:41:08 -0700

    Add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns th elength of a string
+ * @s: string to evaluate
+ * Return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
