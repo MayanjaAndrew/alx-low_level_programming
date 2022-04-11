3d9bf2ca5e8cf122ebed991bc2022998919b4a50
mayanja_andrew <68685849+MayanjaAndrew@users.noreply.github.com>
2022-04-11 22:18:55 +0300

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
  * dog_t - Typedef for struct dog
  */
@@ -23,3 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
